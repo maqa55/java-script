b6223ee185465d448bbc7fc36d08dfe9c302426d
maqa55 <maqamet8@gmail.com>
2024-11-12 15:45:49 +0300

    commit

---
+++ j.js
@@ -0,0 +1,92 @@
+"use strict"
+
+// let lesson = ['cup', 'plate', 'spoon', 'knife', 'fork'];
+
+// for (let i = 0; i < lesson.length; i++) {
+//     console.log(lesson[i]);
+// }
+
+// let lesson = ['cup', 'plate', 'spoon', 'knife', 'fork'];
+
+// for (let i in lesson) {
+//     console.log(lesson[i]);
+// }
+
+// let lesson = ['cup', 'plate', 'spoon', 'knife', 'fork'];
+
+// for (let i of lesson) {
+//     console.log(i);
+// }
+
+// let vagetables = ['xiyar', 'pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni'];
+// for (let i = 0; i < vagetables.length; i++) {
+//     if (vagetables[i][0].toLowerCase() == "s")
+//         console.log(vagetables[i]);
+// }
+
+// function people(ad){
+//     return `Salam hormetli ${ad}`
+// }
+
+// console.log(people("Maqa"));
+
+// ---------------------------------settime------------------------------------------------------------------------
+
+
+// setTimeout(() => {
+//     console.log("bes sayine bitdi");
+
+
+
+
+
+// }, 5000);
+// let a=65.4345;
+// console.log(Math.round(a));
+
+
+
+
+
+
+// let a=Math.round(Math.random()*(10-0)+1);
+// console.log(a);
+
+
+
+
+
+
+// let dt=new Date();
+// console.log(dt);
+
+
+
+
+
+
+// let dt = new Date();
+// console.log(dt.getHours());
+// console.log(dt.getMinutes());
+// console.log(dt.getMilliseconds());
+
+
+
+
+
+
+
+// let dt = new Date('2025,11,12');
+// console.log(dt.getDay());
+
+
+
+
+
+// let a = 100;
+// for (let i = 0; i <= a; i--) { 
+// }                                       sehv
+// setInterval(() => {
+//     console.log(a);
+
+// }, 1000);
